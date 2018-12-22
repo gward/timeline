2b76d7cd0debfdee67626bda0faaaeb05c30c585
Greg Ward <greg@gerg.ca>
2018-12-21 20:55:55 -0500

    Read project description from server-side JSON at startup.

--- src/App.js
+++ src/App.js
@@ -3,58 +3,33 @@ import React, { Component } from 'react';
 import './App.css';
 import { ThumbnailList } from './ThumbnailList';
 
-var project = {
-  'views': [
-    {
-      id: '01.hall',
-      desc: 'hall, from top of stairs',
-      thumbnail: 'thumbnails/01.hall.jpg',
-      photos: [
-        {date: '2018-10-22', path: 'photos/01.hall/2018-10-22.jpg'},
-        {date: '2018-10-23', path: 'photos/01.hall/2018-10-22.jpg'},
-        {date: '2018-10-24', path: 'photos/01.hall/2018-10-24.jpg'},
-        {date: '2018-10-25', path: 'photos/01.hall/2018-10-25.jpg'},
-        {date: '2018-10-26', path: 'photos/01.hall/2018-10-25.jpg'},
-        {date: '2018-10-27', path: 'photos/01.hall/2018-10-25.jpg'},
-        {date: '2018-10-28', path: 'photos/01.hall/2018-10-25.jpg'},
-        {date: '2018-10-29', path: 'photos/01.hall/2018-10-29.jpg'},
-      ],
-    },
-    {
-      id: '02.to-sw',
-      desc: 'towards southwest window',
-      thumbnail: 'thumbnails/02.to-sw.jpg',
-      photos: [
-        {date: '2018-10-22', path: 'photos/02.to-sw/2018-10-22.jpg'},
-        {date: '2018-10-23', path: 'photos/02.to-sw/2018-10-22.jpg'},
-        {date: '2018-10-24', path: 'photos/02.to-sw/2018-10-24.jpg'},
-        {date: '2018-10-25', path: 'photos/02.to-sw/2018-10-25.jpg'},
-        {date: '2018-10-26', path: 'photos/02.to-sw/2018-10-26.jpg'},
-      ],
-    },
-    {
-      id: '03.from-sw',
-      desc: 'from southwest window',
-      thumbnail: 'thumbnails/03.from-sw.jpg',
-      photos: [
-        {date: '2018-10-22', path: 'photos/03.from-sw/2018-10-22.jpg'},
-        {date: '2018-10-23', path: 'photos/03.from-sw/2018-10-22.jpg'},
-        {date: '2018-10-24', path: 'photos/03.from-sw/2018-10-24.jpg'},
-        {date: '2018-10-25', path: 'photos/03.from-sw/2018-10-25.jpg'},
-        {date: '2018-10-26', path: 'photos/03.from-sw/2018-10-26.jpg'},
-        {date: '2018-10-27', path: 'photos/03.from-sw/2018-10-26.jpg'},
-        {date: '2018-10-28', path: 'photos/03.from-sw/2018-10-26.jpg'},
-        {date: '2018-10-29', path: 'photos/03.from-sw/2018-10-29.jpg'},
-      ],
-    },
-  ],
-}
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {project: null};
+  }
+
+  componentDidMount() {
+    fetch('project.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((project) => {
+        this.setState({project});
+      });
+  }
+
   render() {
+    if (this.state.project === null) {
+      return null;
+    }
+
     return (
       <div className="container">
-        <ThumbnailList project={project} />
+        <ThumbnailList project={this.state.project} />
       </div>
     );
   }
