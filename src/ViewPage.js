2d5f7fce0b0d5d17cb5efa5c4e5c7a432759682d
Greg Ward <greg@gerg.ca>
2018-12-23 15:13:44 -0500

    Add action buttons; implement Slower and Faster.

--- src/ViewPage.js
+++ src/ViewPage.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import { ButtonGroup, Button } from 'reactstrap';
 
 export class ViewPage extends Component {
   constructor(props) {
     super(props);
     this.timeout = null;
-    this.delay = 3000;          // ms per day
-    this.state = {photoIndex: -1, last: false};
+    this.state = {
+      photoIndex: -1,
+      last: false,
+      delay: 3000,              // ms per day
+    };
 
-    this.nextPhoto = this.nextPhoto.bind(this);
+    this.nextDay = this.nextDay.bind(this);
+    this.actionSlower = this.actionSlower.bind(this);
+    this.actionFaster = this.actionFaster.bind(this);
   }
 
   componentDidMount() {
-    this.nextPhoto();
+    this.nextDay();
   }
 
   componentWillUnmount() {
@@ -31,18 +37,41 @@ export class ViewPage extends Component {
       <div>
         <h1>{photos[index].date}: {desc}{last}</h1>
         <img style={{width: "100%"}} src={photos[index].path} alt="" />
+        <ButtonGroup>
+          <Button>Backwards</Button>
+          <Button onClick={this.actionSlower}>Slower</Button>
+          <Button>Pause</Button>
+          <Button
+            onClick={this.actionFaster}
+            disabled={this.state.delay < 500}>Faster</Button>
+          <Button>Forwards</Button>
+        </ButtonGroup>
+        <span>(current speed: {this.state.delay/1000} sec/day}</span>
       </div>
     );
   }
 
-  nextPhoto() {
+  nextDay() {
     let photos = this.props.view.photos;
     let index = this.state.photoIndex;
     let last = (index + 1 === photos.length - 1);
     this.setState({photoIndex: index + 1, last})
 
     if (!last) {
-      this.timeout = setTimeout(this.nextPhoto, this.delay);
+      this.timeout = setTimeout(this.nextDay, this.state.delay);
     }
+  }
+
+  actionSlower() {
+    this.setState({delay: this.state.delay + 500});
+  }
+
+  actionFaster() {
+    let delay = this.state.delay;
+    if (delay < 500) {
+      return;
+    }
+    delay -= 500;
+    this.setState({delay});
   }
 }
