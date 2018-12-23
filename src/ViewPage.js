0823d4f67dba66ec8c71a8500744452ab038c545
Greg Ward <greg@gerg.ca>
2018-12-23 16:01:19 -0500

    Implement Reverse/Forwards buttons.

--- src/ViewPage.js
+++ src/ViewPage.js
@@ -8,10 +8,14 @@ export class ViewPage extends Component {
     this.state = {
       photoIndex: -1,
       last: false,
+      forward: true,            // direction of playback
       delay: 3000,              // ms per day
     };
 
+    this.previousDay = this.previousDay.bind(this);
     this.nextDay = this.nextDay.bind(this);
+    this.actionReverse = this.actionReverse.bind(this);
+    this.actionForward = this.actionForward.bind(this);
     this.actionSlower = this.actionSlower.bind(this);
     this.actionFaster = this.actionFaster.bind(this);
     this.actionRestart = this.actionRestart.bind(this);
@@ -39,13 +43,13 @@ export class ViewPage extends Component {
         <h1>{photos[index].date}: {desc}{last}</h1>
         <img style={{width: "100%"}} src={photos[index].path} alt="" />
         <ButtonGroup>
-          <Button>Backwards</Button>
+          <Button onClick={this.actionReverse}>Reverse</Button>
           <Button onClick={this.actionSlower}>Slower</Button>
           <Button>Pause</Button>
           <Button
             onClick={this.actionFaster}
             disabled={this.state.delay < 500}>Faster</Button>
-          <Button>Forwards</Button>
+          <Button onClick={this.actionForward}>Forward</Button>
           <Button onClick={this.actionRestart}>Restart</Button>
         </ButtonGroup>
         <span>(current speed: {this.state.delay/1000} sec/day}</span>
@@ -53,19 +57,53 @@ export class ViewPage extends Component {
     );
   }
 
+  previousDay() {
+    let index = this.state.photoIndex;
+    let last = false;
+    //console.log(`previousDay(): index = ${index}, last = ${last}`);
+    if (index <= 0) {
+      return;
+    }
+    this.setState({photoIndex: index - 1, last});
+    this.timeout = setTimeout(this.previousDay, this.state.delay);
+  }
+
   nextDay() {
     let photos = this.props.view.photos;
     let index = this.state.photoIndex;
     let last = (index + 1 === photos.length - 1);
-    this.setState({photoIndex: index + 1, last})
+    //console.log(`nextDay(): index = ${index}, last = ${last}`);
+    this.setState({photoIndex: index + 1, last});
 
     if (!last) {
       this.timeout = setTimeout(this.nextDay, this.state.delay);
     }
   }
 
+  actionReverse() {
+    if (!this.state.forward) {  // already going backwards
+      return;
+    }
+
+    clearTimeout(this.timeout);
+    this.setState({forward: false});
+    this.timeout = setTimeout(this.previousDay, this.state.delay);
+  }
+
+  actionForward() {
+    if (this.state.forward) {   // already going forward
+      return;
+    }
+
+    clearTimeout(this.timeout);
+    this.setState({forward: true});
+    this.timeout = setTimeout(this.nextDay, this.state.delay);
+  }
+
   actionSlower() {
-    this.setState({delay: this.state.delay + 500});
+    let delay = this.state.delay;
+    delay += 500;
+    this.setState({delay});
   }
 
   actionFaster() {
@@ -79,7 +117,7 @@ export class ViewPage extends Component {
 
   actionRestart() {
     clearTimeout(this.timeout);
-    this.setState({photoIndex: -1});
+    this.setState({photoIndex: -1, forward: true});
     this.timeout = setTimeout(this.nextDay, 0);
   }
 }
